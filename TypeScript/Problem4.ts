249d83f3d50dffcc552fa3079ceeba993773669c
CristianLopezR <cristian.rueda@encora.com>
2025-07-13 01:13:53 -0400

    Update Problem4.ts

--- TypeScript/Problem4.ts
+++ TypeScript/Problem4.ts
@@ -1,14 +1,16 @@
 function isMatch(s: string, p: string): any {
     let memo:Map<string,boolean>=new Map();
     return backTrack(s,p,0,0,memo);
-    
+}
 
 
 function backTrack(s:string,p:string,i:number,j:number,memo:Map<string,boolean>):any{
-    let key:string=i+""+j;
+    let key:string=i + "" + j;
         if(memo.has(key)) return memo.get(key);
+
         let result:boolean;
-        if (i<s.length && j>=p.length) return true;
+
+        if (i>=s.length && j>=p.length) return true;
         if (j>=p.length) return false
 
         let match:boolean = i <s.length && (s.charAt(i)==p.charAt(j) || p.charAt(j)==".");
@@ -17,8 +19,7 @@ function backTrack(s:string,p:string,i:number,j:number,memo:Map<string,boolean>)
             memo.set(key,result);
             return result;
         }
-        result = match && backTrack(s,p,i+1,j,memo);
+        result = match && backTrack(s,p,i+1,j+1,memo);
         memo.set(key,result);
         return result;
-}
 }
